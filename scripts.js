3df9a565f4ee9f42c7fc4fb6583b245f40340d96
happyhar <65075165+happyhar@users.noreply.github.com>
2021-07-08 12:05:51 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+// function fun(){
+//     document.getElementById("container").style.backgroundImage = "url('model-y.jpg')";
+// }
+// for(i=1;i<=2;i++){
+//     document.write("<div id='container"+i+"'>Helllo</div>");
+//     document.getElementById("container"+i+"").style.backgroundImage = "url('model-y.jpg')";
+    
+// }
+
+// console.log("hekjk");
+// var index = 1;
+
+// nextSlide();
+// function nextSlide() {
+//     // document.getElementById("container").style.backgroundImage = "url('model-y.jpg')";
+//     console.log(index);
+//     index++;
+//     setTimeout(nextSlide, 3000);
+// }
